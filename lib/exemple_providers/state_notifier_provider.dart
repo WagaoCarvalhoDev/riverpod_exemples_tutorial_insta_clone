c9c9204bfa689af939daec388b2905b1de62504f
wagner <wagaocarvalhodev@gmail.com>
2023-02-11 12:38:54 -0300

    state_notifier_provider.dart

--- lib/exemple_providers/state_notifier_provider.dart
+++ lib/exemple_providers/state_notifier_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-extension OptionalInfixAddition<T extends num> on T {
+extension OptionalInfixAddition<T extends num> on T? {
   T? operator +(T? other) {
     final shadow = this;
     if (shadow != null) {
@@ -30,11 +30,20 @@ extension OptionalInfixAddition<T extends num> on T {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class CounterProvider extends StateNotifier<int?> {
+  CounterProvider() : super(null);
+
+  increment() => state = state == null ? 1 : state + 1;
+}
+
+final counterProvider =
+    StateNotifierProvider<CounterProvider, int?>((ref) => CounterProvider());
+
+class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('widget.title'),
@@ -43,7 +52,18 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('data'),
+            Consumer(builder: (context, ref, child) {
+              final counter = ref.watch(counterProvider);
+              final text =
+                  counter == null ? 'Press Button' : counter.toString();
+              return Text(text);
+            }),
+            ElevatedButton(
+              onPressed: () {
+                ref.read(counterProvider.notifier).increment();
+              },
+              child: Text('Increment'),
+            ),
           ],
         ),
       ),
