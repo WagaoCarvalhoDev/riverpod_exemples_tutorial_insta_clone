a5bd85179d95343a151c70650dba44c635e08f9e
wagner <wagaocarvalhodev@gmail.com>
2023-02-14 17:25:05 -0300

    class Film

--- lib/exemple_providers/films_filter_provider.dart
+++ lib/exemple_providers/films_filter_provider.dart
@@ -19,6 +19,48 @@ class MyApp extends StatelessWidget {
   }
 }
 
+@immutable
+class Film {
+  final String id;
+  final String title;
+  final String description;
+  final bool isFavorite;
+
+  const Film({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.isFavorite,
+  });
+
+  Film copy({required bool isFavorite}) {
+    return Film(
+      id: id,
+      title: title,
+      description: description,
+      isFavorite: isFavorite,
+    );
+  }
+
+  @override
+  String toString() => 'Film(id: $id, '
+      'title: $title, '
+      'description: $description, '
+      'isFavorite: $isFavorite)';
+
+  @override
+  bool operator ==(covariant Film other) =>
+      id == other.id && isFavorite == other.isFavorite;
+
+  @override
+  int get hashCode => Object.hashAll(
+        [
+          id,
+          isFavorite,
+        ],
+      );
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
